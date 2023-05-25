fee7b5e128a2ee5f46f7fd8aaa4682cd28ba1bc8
Filipe Ferreira Paulo <filipef.paulo@gmail.com>
2023-05-24 21:07:01 -0300

    feat: Add call for API to retrive all posts

--- src/actions/codeLeapAPI.ts
+++ src/actions/codeLeapAPI.ts
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
+import { PostInterface } from '../interfaces/post.interface';
+
 const codeLeapAPI = axios.create({ baseURL: 'https://dev.codeleap.co.uk/careers/' });
 
-interface Post {
-  id: number;
-  title: string;
-  username: string;
-  content: string;
-  created_datetime: string;
-}
-
-export async function createPost({ title, username, content }: Partial<Post>) {
+export async function createPost({ title, username, content }: Partial<PostInterface>) {
   return codeLeapAPI
     .post('', { title, username, content })
+    .then((response) => response.data)
+    .catch((error) => error);
+}
+
+export async function getPosts() {
+  return codeLeapAPI
+    .get('')
     .then((response) => response.data)
     .catch((error) => error);
 }
---
+++ src/interfaces/post.interface.ts
@@ -0,0 +1,7 @@
+export interface PostInterface {
+  id: number;
+  title: string;
+  username: string;
+  content: string;
+  created_datetime: string;
+}
