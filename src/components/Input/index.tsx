18532621295a859a803e3b7f90c3315b1f267422
Filipe Ferreira Paulo <filipef.paulo@gmail.com>
2023-05-25 22:56:51 -0300

    refac: Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -18,7 +18,7 @@ export function Input({
       <input
         id={labelName}
         className={cx(
-          'mt-2 mb-4 border-[1px] border-zinc-500 px-4 py-2 rounded-lg w-full',
+          'mb-4 mt-2 w-full rounded-lg border-[1px] border-zinc-500 px-4 py-2',
           className,
         )}
         {...inputElements}
