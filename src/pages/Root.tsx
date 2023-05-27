ea8242f686df2b085877e514bfde6e8369db2cb3
Filipe Ferreira Paulo <filipef.paulo@gmail.com>
2023-05-27 17:03:52 -0300

    fix: Fix state update error in Root component

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { useDispatch } from '../hooks/useRedux';
@@ -5,11 +6,17 @@ import { setUsername } from '../redux/features/username.slice';
 
 export function Root() {
   const username = localStorage.getItem('username');
-
-  if (!username) return <Navigate to="/login" />;
-
   const dispatch = useDispatch();
-  dispatch(setUsername(username));
+
+  useEffect(() => {
+    if (username) {
+      dispatch(setUsername(username));
+    }
+  }, [username, dispatch]);
+
+  if (!username) {
+    return <Navigate to="/login" />;
+  }
 
   return <Navigate to="/home" />;
 }
